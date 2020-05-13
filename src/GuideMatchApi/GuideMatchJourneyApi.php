200b3d398f5fd1072dfcbee2af27fa4dfb992505
adrianmos1980 <adrian.mos@cognizant.com>
2020-05-13 22:17:59 +0300

    Add Agreements Service Api, Unit Tests - coding standardse

--- src/GuideMatchApi/GuideMatchJourneyApi.php
+++ src/GuideMatchApi/GuideMatchJourneyApi.php
@@ -3,29 +3,31 @@
 declare(strict_types=1);
 
 namespace App\GuideMatchApi;
+
 use Symfony\Component\HttpClient\HttpClient;
 use \Exception;
 
-class GuideMatchJourneyApi {
-    
+class GuideMatchJourneyApi
+{
+
 
     /**
      * Start Guide Match Journey
-     * 
+     *
      * @param string $base_api_url
      * @param strig  $search_by
      * @return array
      */
     
-    public function getJourneyUuid($base_api_url, string $search_by){
-
-        if(empty($base_api_url) && empty($qsearch_by)){
+    public function getJourneyUuid($base_api_url, string $search_by)
+    {
+        if (empty($base_api_url) && empty($qsearch_by)) {
             throw new Exception('Invalid arguments of method');
         }
 
 
         $client = HttpClient::create();
-        $response = $client->request('GET', $base_api_url."/scale/decision-tree/journeys",[
+        $response = $client->request('GET', $base_api_url."/scale/decision-tree/journeys", [
             'query' => [
                 'q' => $search_by,
             ]
@@ -38,16 +40,16 @@ class GuideMatchJourneyApi {
 
     /**
      * Get first set of Questions from Guide Match Journey
-     * 
+     *
      * @param string $base_api_url
      * @param string $journey_uuid - Journey instance id
-     * @param string $question_uuid - Unique identifier of the question answered in this response (if a set of questions, the id of the first question in the set)
-     * 
+     * @param string $question_uuid - Unique identifier of the question answered
+     *
      * @return array
      */
-    public function getQuestions($base_api_url, $journey_uuid, $questions_uuid){
-
-        if(empty($base_api_url) && empty($q)){
+    public function getQuestions($base_api_url, $journey_uuid, $questions_uuid)
+    {
+        if (empty($base_api_url) && empty($q)) {
             throw new Exception('Invalid arguments of method');
         }
 
@@ -56,29 +58,28 @@ class GuideMatchJourneyApi {
         $content = $response->getContent();
         $content = $response->toArray();
         return $content;
-
     }
     
     /**
      * Send to API endpoint the user answer and get the next questions or Comercial Agreements or a flag to call Suport
      * for further help
-     * 
+     *
      * @param string $base_api_url
      * @param string $journey_uuid  - Journey instance id
-     * @param string $question_uuid - Unique identifier of the question answered in this response (if a set of questions, the id of the first question in the set)
+     * @param string $question_uuid - Unique identifier of the question answered in this response
      * @param array $question_response_id - Uuid question answered by user
-     * 
+     *
      * @return array
      */
 
-    public function getDecisionTree(string $base_api_url, string $journey_uuid, string $question_uuid, array $question_response){
-
-        if(
-            empty($base_api_url) && 
+    public function getDecisionTree(string $base_api_url, string $journey_uuid, string $question_uuid, array $question_response)
+    {
+        if (
+            empty($base_api_url) &&
             empty($journey_uuid) &&
-            empty($question_uuid) && 
+            empty($question_uuid) &&
             empty($question_response)
-        ){
+        ) {
             throw new Exception('Invalid arguments of method');
         }
 
@@ -86,35 +87,30 @@ class GuideMatchJourneyApi {
             "data"=> $question_response
         ];
 
-
         $client = HttpClient::create();
 
         $response = $client->request('POST', "{$base_api_url}/scale/decision-tree/journeys/{$journey_uuid}/questions/{$question_uuid}/outcome", [
             'json' => $data
-        
         ]);
         
         $content = $response->getContent();
         $content = $response->toArray();
         return $content;
-
     }
     /**
      * Get summary of Guide Match Journey
-     * 
+     *
      * @param string $base_api_url
      * @param $journey_uuid - Journey instance id
-     * @return array 
+     * @return array
      */
 
-    public function getJourneySummary($base_api_url,$journey_uuid){
-
+    public function getJourneySummary($base_api_url, $journey_uuid)
+    {
         $client = HttpClient::create();
         $response = $client->request('GET', "{$base_api_url}/ourney-summaries/{$journey_uuid}");
         $content = $response->getContent();
         $content = $response->toArray();
-        return $content;        
-
+        return $content;
     }
-
 }
--- tests/App/GuideMatchApi/GuideMatchJourneyApiTest.php
+++ tests/App/GuideMatchApi/GuideMatchJourneyApiTest.php
@@ -1,25 +1,27 @@
 <?php
 
 namespace App\Tests\App\GuideMatchyApi;
+
 use PHPUnit\Framework\TestCase;
 use App\GuideMatchApi\GuideMatchJourneyApi;
 
-class GuideMatchJourneyApiTest extends TestCase{
-
-    public function testGetJourneyUuid(){
-
+class GuideMatchJourneyApiTest extends TestCase
+{
+    public function testGetJourneyUuid()
+    {
+        $search_by = 'linen';
         $api = new GuideMatchJourneyApi();
         $base_api_url =  getenv('GUIDE_MATCH_DECISION_TREE_API');
-        $response = $api->getJourneyUuid($base_api_url, 'linen');
+        $response = $api->getJourneyUuid($base_api_url, $search_by);
         $this->assertTrue(!empty($response));
-        $this->assertTrue(!empty($response[0]));        
+        $this->assertTrue(!empty($response[0]));
         $this->assertArrayHasKey('uuid', ($response[0]));
         $this->assertArrayHasKey('questionUuid', ($response[0]));
     }
 
 
-    public function testGetQuestion(){
-
+    public function testGetQuestion()
+    {
         $api = new GuideMatchJourneyApi();
         $base_api_url =  getenv('GUIDE_MATCH_DECISION_TREE_API');
         $response = $api->getJourneyUuid($base_api_url, 'linen');
@@ -27,17 +29,16 @@ class GuideMatchJourneyApiTest extends TestCase{
         $this->assertArrayHasKey('uuid', ($response[0]));
         $this->assertArrayHasKey('questionUuid', ($response[0]));
 
-        $questions= $api->getQuestions($base_api_url,$response[0]['uuid'],$response[0]['questionUuid']);
+        $questions= $api->getQuestions($base_api_url, $response[0]['uuid'], $response[0]['questionUuid']);
         $this->assertTrue(!empty($questions));
         $this->assertTrue(!empty($questions));
         $this->assertArrayHasKey('uuid', ($questions));
         $this->assertArrayHasKey('definedAnswers', ($questions));
-
     }
 
 
-    public function testGetDecisionTree(){
-
+    public function testGetDecisionTree()
+    {
         $search_by = 'linen';
 
         $api = new GuideMatchJourneyApi();
@@ -51,7 +52,7 @@ class GuideMatchJourneyApiTest extends TestCase{
         $questionUuid = $start_journery_api_response[0]['questionUuid'];
 
         //get first set of questions
-        $get_questions_api_response = $api->getQuestions($base_api_url,$uuid,$questionUuid);
+        $get_questions_api_response = $api->getQuestions($base_api_url, $uuid, $questionUuid);
         $this->assertTrue(!empty($get_questions_api_response));
         $this->assertArrayHasKey('uuid', ($get_questions_api_response));
         $this->assertArrayHasKey('definedAnswers', ($get_questions_api_response));
@@ -63,14 +64,14 @@ class GuideMatchJourneyApiTest extends TestCase{
         $user_response = $definedAnswers[$response_question_number]['uuid'];
 
         //get api response
-        $decision_tree_api_response =  $api->getDecisionTree($base_api_url,$uuid, $questionUuid, [$user_response]);
+        $decision_tree_api_response =  $api->getDecisionTree($base_api_url, $uuid, $questionUuid, [$user_response]);
         $this->assertTrue(!empty($decision_tree_api_response));
         $this->assertArrayHasKey('outcomeType', $decision_tree_api_response);
 
-        if($decision_tree_api_response['outcomeType'] == 'lot'){
-            $this->assertArrayHasKey('agreementId',$decision_tree_api_response['data'][0]);
-            $this->assertArrayHasKey('agreementName',$decision_tree_api_response['data'][0]);
-        }else{
+        if ($decision_tree_api_response['outcomeType'] == 'lot') {
+            $this->assertArrayHasKey('agreementId', $decision_tree_api_response['data'][0]);
+            $this->assertArrayHasKey('agreementName', $decision_tree_api_response['data'][0]);
+        } else {
             $this->assertArrayHasKey('definedAnswers', ($decision_tree_api_response['data']));
         }
     }
