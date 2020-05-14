1ca359d90932a6d75f3f6573ed135922cb49e9e8
adrianmos1980 <adrian.mos@cognizant.com>
2020-05-14 13:34:00 +0300

    Testing Api script

---
+++ src/Controller/GuideMatchJourneyTestController.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use App\Models\TestJourneyApi;
+
+
+class GuideMatchJourneyTestController extends AbstractController
+{
+
+    
+    public function guideMatchJourney(Request $request)
+    {
+
+        $q =$request->query->get('q');
+
+
+        dump($q);
+        $test_model = new TestJourneyApi();
+        $test_model->testJourney($q);
+        die();
+     }
+
+}
+?>
