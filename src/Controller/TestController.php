1a1203304ed02a7b7f6b2ed89881feea3752ce2f
adrianmos1980 <adrian.mos@cognizant.com>
2020-05-19 14:15:05 +0300

    test redirect

---
+++ src/Controller/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+ 
+ class TestController extends  AbstractController{
+
+    public function index(){
+        return new Response('Test Redirect');
+    }
+
+ }
