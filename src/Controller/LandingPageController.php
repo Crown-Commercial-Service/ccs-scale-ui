082dc6d6f839999d7a40d79a136351b2fbac1c7d
adrianmos1980 <adrian.mos@cognizant.com>
2020-05-26 09:26:09 +0300

    Add Landing Page Controller

---
+++ src/Controller/LandingPageController.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpClient\HttpClient;
+use App\Models\GuideMatchModel;
+
+
+class LandingPageController extends AbstractController
+{
+
+    public function landingPage(Request $request)
+    {
+        $q =$request->query->get('q');
+        $httpClient = HttpClient::create();
+        $model = new GuideMatchModel($httpClient, getenv('GUIDE_MATCH_DECISION_TREE_API'), $q);
+        $journeyData = $model->getJurneyData();
+
+       
+     }
+
+}
+?>
