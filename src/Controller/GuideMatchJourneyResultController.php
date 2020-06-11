903cad7b3b555ffaeeb36c21eed3bbc1ebc8128b
adrianmos1980 <adrian.mos@cognizant.com>
2020-06-11 13:02:56 +0300

    guide match results page: SFC-93, SFC-95, SFC-96, SFC-89, SFC-292

--- src/Controller/GuideMatchJourneyResultController.php
+++ src/Controller/GuideMatchJourneyResultController.php
@@ -6,19 +6,48 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpClient\HttpClient;
-use App\Models\GuideMatchJourneyModel;
-use App\GuideMatchApi\GuideMatchJourneyApi;
 use Symfony\Component\HttpFoundation\Request;
+use App\Models\Decrypt;
+use App\Models\Encrypt;
+
+use App\Models\UserAnswers;
+use App\Models\GuideMatchAgreementModel;
+use App\GuideMatchApi\ServiceAgreementsApi;
+
 
 class GuideMatchJourneyResultController extends AbstractController
 {
-    public function journeyResult(Request $request, $journeyId, $journeyInstanceId, $journeyData)
+    public function journeyResult(Request $request, string $journeyId, string $journeyInstanceId, string $searchBy, string $journeyData)
     {
-        die('x');
-               
+       
+        $decrypt = new Decrypt(urldecode($journeyData));
+        $journeyDataDecrypt = json_decode($decrypt->getDecryptedString(), true);
+        $userAnswered = new UserAnswers($journeyDataDecrypt['historyAnswered']);
 
-        return $this->render('pages/result_page.twig', [
-           
+        $httpClient = HttpClient::create();
+        $agreementsApi  = new ServiceAgreementsApi($httpClient, getenv('AGREEMENTS_SERVICE_ROOT_URL'));
+        $agrementModel = new GuideMatchAgreementModel($agreementsApi, $journeyDataDecrypt['agreementData']);
+        $frameworks = $agrementModel->getAgreements();
+        $lots = $agrementModel->getLotsData();
+
+        $encrypt = new Encrypt(json_encode($journeyDataDecrypt['historyAnswered']));
+       
+        $journeyHistory =  urlencode($encrypt->getEncryptedString());
+
+        return $this->render('pages/result_page.html.twig', [
+            'searchBy' => $searchBy,
+            'historyAnswered' => $userAnswered->formatForView(),
+            'frameworks' => $frameworks,
+            'countFrameworks' => count($frameworks),
+            'lotsData' => $lots,
+            'journeyId' => $journeyId,
+            'journeyInstanceId' => $journeyInstanceId,
+            'journeyHistory' => $journeyHistory,
+            'agreementsNames' => $agrementModel->getAgreementsNames()
+            
         ]);
     }
+
+
+
 }
