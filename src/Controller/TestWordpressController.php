37a2a6cf23cbf8dcc1df3ee93d180359d519c421
Favian Poputa <favian.poputa@cognizant.com>
2020-06-18 10:12:49 +0300

    add more stuff t test

--- src/Controller/TestWordpressController.php
+++ src/Controller/TestWordpressController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Request;
 use Studio24\Frontend\Cms\Wordpress;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,11 +16,28 @@ class TestWordpressController extends AbstractController
     {
         $q = $request->query->get('q');
         if (!empty($q)) {
-            $api = new Wordpress('https://webdev-cms.crowncommercial.gov.uk/wp-json/');
+            $url = ($q == 1) ? 'https://webdev-cms.crowncommercial.gov.uk/wp-json': 'https://webdev-cms.crowncommercial.gov.uk';
+
+            $api = new Wordpress($url);
             $id = 22;
             $menu = $api->getMenu($id);
             return $this->json($menu);
         }
+
+        return new Response('AT LEAST IT SHOULD RETURN THIS CONTROLLER ');
+
+    }
+
+    public function curl(Request $request)
+    {
+        $q = $request->query->get('q');
+        if (!empty($q)) {
+            $client = HttpClient::create();
+            $url = ($q == 1) ? 'https://webdev-cms.crowncommercial.gov.uk': 'https://webdev-cms.crowncommercial.gov.uk/wp-json/';
+            $response = $client->request('GET', $url);
+            $this->json($response);
+        }
+
 
         return new Response('AT LEAST IT SHOULD RETURN THIS CONTROLLER ');
 
