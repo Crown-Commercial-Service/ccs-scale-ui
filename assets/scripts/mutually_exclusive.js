517f2e9689f6b5f3382e46342687fcaf068accd9
Favian Poputa <favian.poputa@cognizant.com>
2020-06-23 21:28:46 +0300

    check all feature / partial developed because we don't really know how a simple select answer array looks yet

--- assets/scripts/mutually_exclusive.js
+++ assets/scripts/mutually_exclusive.js
@@ -10,3 +10,11 @@ function unExlcusive() {
     var checkbox = document.getElementsByClassName('e-button')[0];
     checkbox.checked = false;
 }
+
+function toggle(x) {
+    var checkboxes = document.getElementsByClassName('all-options');
+    // console.log(checkboxes);
+    for(var checbox in checkboxes){
+        checkboxes[checbox].checked = x.checked;
+    }
+}
