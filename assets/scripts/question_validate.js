a74a3bad03988f71afebde8d3c565ca30e2d5ca3
Favian Poputa <favian.poputa@cognizant.com>
2020-06-23 17:41:09 +0300

    Javascript separation

--- assets/scripts/question_validate.js
+++ assets/scripts/question_validate.js
@@ -1,14 +1,16 @@
 function validate() {
-    console.log(document.question);
     var return_value = false;
+    var formLayout   = document.getElementById('form-layout');
+    var err          = document.getElementById('changed-name-error');
+
     for (var i = 0; i < document.getElementsByClassName('v-selector').length; i++) {
         if (document.getElementsByClassName('v-selector')[i].checked) return_value = true;
     }
-    var formLayout = document.getElementById('form-layout');
-    var err = document.getElementById('changed-name-error');
+
     if (return_value === false) {
         formLayout.classList.add('govuk-form-group--error');
         err.style.display = 'block';
     }
+
     return return_value;
 }
