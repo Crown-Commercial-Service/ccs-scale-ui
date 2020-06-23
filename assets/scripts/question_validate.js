74e9c4cf5b603ff83912c7f5526b4cd4da3296bf
Favian Poputa <favian.poputa@cognizant.com>
2020-06-24 00:00:34 +0300

    javascript validation for conditional input

--- assets/scripts/question_validate.js
+++ assets/scripts/question_validate.js
@@ -1,16 +1,44 @@
+
+// check if element has class
+function hasClass(element, className) {
+    return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
+}
+
+// check if string is empty or null or empty space
+function isEmptyOrSpaces(str) {
+    return str === null || str.match(/^ *$/) !== null;
+}
+
 function validate() {
-    var return_value = false;
+    var returnValue  = false;
     var formLayout   = document.getElementById('form-layout');
-    var err          = document.getElementById('changed-name-error');
+    var err          = document.getElementById('no-selection');
+    var errInput     = document.getElementById('no-input');
+    var inputs       = document.getElementsByClassName('conditional-input');
 
     for (var i = 0; i < document.getElementsByClassName('v-selector').length; i++) {
-        if (document.getElementsByClassName('v-selector')[i].checked) return_value = true;
+        if (document.getElementsByClassName('v-selector')[i].checked) returnValue = true;
     }
 
-    if (return_value === false) {
+    if (returnValue === false) {
         formLayout.classList.add('govuk-form-group--error');
         err.style.display = 'block';
     }
 
-    return return_value;
+    // I check all conditional inputs if one is not hidden
+    for(var i in inputs) {
+        if (Number.isInteger(Number(i)) == true) {
+            if (!hasClass(inputs[i], 'govuk-radios__conditional--hidden')) {
+                if (isEmptyOrSpaces(inputs[i].firstElementChild.childNodes[5].value)) {
+                    returnValue = false;
+                    inputs[i].firstElementChild.childNodes[5].classList.add('govuk-input--error');
+                    formLayout.classList.add('govuk-form-group--error');
+                    inputs[i].style.borderLeftColor ='#b10e1e';
+                    errInput.style.display = 'block';
+                }
+            }    
+        }
+    }
+
+    return returnValue;
 }
