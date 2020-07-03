c6736b1bb8f7fc782edcfb97b4c5ec4354f186c1
Favian Poputa <favian.poputa@cognizant.com>
2020-07-03 09:48:28 +0300

    temporal fix

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -15,9 +15,15 @@ class AppExtension extends AbstractExtension
 
     public function modifyUrl($string)
     {
+        // This is a temporaly fix untill links from wordpress are corectly rendered
+        if (strpos($string, 'https://webdev-cms.crowncommercial.gov.uk')) {
+            return str_replace('https://webdev-cms', 'https://www', $string);
+        }
+
         if ($string[0] == '/') {
             return "https://www.crowncommercial.gov.uk" . $string;
         }
+        
         return $string;
-    }   
+    }
 }
