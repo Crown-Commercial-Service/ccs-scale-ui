f48f36c0ed79cbe5ad63ee773b0aaa1aa4d4949f
adrianmos1980 <adrian.mos@cognizant.com>
2020-07-16 15:38:28 +0300

    add contact css url

---
+++ src/Controller/GuideMatchContactCssController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
+use App\Models\GuideMatchJourneyModel;
+use App\GuideMatchApi\GuideMatchJourneyApi;
+use Symfony\Component\HttpFoundation\Request;
+use App\Models\Decrypt;
+
+class GuideMatchContactCssController extends AbstractController
+{
+    public function contactCss(Request $request, $journeyId, $journeyInstanceId, $journeyHistory, $gPage)
+    {
+
+        return $this->render('pages/contact_css.html.twig', [
+           
+        ]);
+        
+    }
+}
