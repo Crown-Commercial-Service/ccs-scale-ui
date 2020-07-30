66ab0158822974d75396b3ab114d36cc3d688581
adrianmos1980 <adrian.mos@cognizant.com>
2020-07-30 13:32:23 +0300

    change legal-tech with laptop term

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -22,14 +22,12 @@ class LandingPageController extends AbstractController
             $journeyId = 'b87a0636-654e-11ea-bc55-0242ac130003';
         } elseif ($q == 'legal') {
             $journeyId = 'ccb5c730-75b5-11ea-bc55-0242ac130003';
-        } elseif ($q == 'legal_tech') {
+        } elseif ($q == 'laptop') {
             $journeyId = 'ccb6174e-75b5-11ea-bc55-0242ac130003';
         //   $q= 'legal';
         } else {
-            die('We have mockups API just for linen and legal.');
+            die('We have mockups API just for linen and legal and laptop.');
         }
-
-        
 
         return $this->render('pages/landing_page.html.twig', [
             'journeyUuid' => $journeyId,
