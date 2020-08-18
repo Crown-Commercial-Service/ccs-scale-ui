a77e1a62399e060a69774359a048a2fc03112057
adrianmos1980 <adrian.mos@cognizant.com>
2020-08-18 16:22:02 +0300

    fix back button, clean debgu code

--- public/index.php
+++ public/index.php
@@ -9,18 +9,12 @@ require dirname(__DIR__) . '/config/bootstrap.php';
 $env = $_SERVER['APP_ENV'] ?? 'prod';
 
 $debug = (bool) ($_SERVER['APP_DEBUG'] ?? ('prod' !== $env));
-$dbg = !empty($_GET['dbg']) ? $_GET['dbg'] : null;
-
 
 if ($debug) {
     umask(0000);
     Debug::enable();
 }
 
-if($dbg == 1){
-
-    dd($_SERVER);
-}
 
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
     Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
@@ -30,10 +24,6 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
     Request::setTrustedHosts([$trustedHosts]);
 }
 
-if($dbg == 2){
-
-    dd($_SERVER);
-}
 
 $kernel = new Kernel($env, $debug);
 
@@ -43,25 +33,12 @@ if ('prod' === $env) {
     $kernel = new CacheKernel($kernel);
 }
 
-if($dbg == 3){
-
-    dd($_SERVER);
-}
 $uri = $_SERVER['REQUEST_URI'];
 
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 
-if($dbg == 4){
-
-    dd($_SERVER);
-}
 $response->send();
-
-if($dbg == 5){
-
-    dd($_SERVER);
-}
 
 $kernel->terminate($request, $response);
 
