7655398441e5850384bef5b8a438ee116466d505
tommyb82 <thomasbunting@gmail.com>
2020-10-28 23:11:54 +0000

    test DNS resolution to private API from fargate

--- public/test.php
+++ public/test.php
@@ -3,9 +3,11 @@ echo 'Test 1';
 echo '<br/>';
 if (!empty($_GET['debug'])) {
     echo '<pre>';
-    
+
     var_dump($_SERVER);
 }
 var_dump($_GET);
 echo 'Hellor world';
+
+file_get_contents("https://o8eqlgajbi-vpce-09bc0b188b0cf0095.execute-api.eu-west-2.amazonaws.com/sbx2");
 ?>
