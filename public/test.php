b32d883dda3d58a8a5455bc80347372048afedb9
tommyb82 <thomasbunting@gmail.com>
2020-10-28 23:39:36 +0000

    update echo gm api

--- public/test.php
+++ public/test.php
@@ -9,5 +9,6 @@ if (!empty($_GET['debug'])) {
 var_dump($_GET);
 echo 'Hellor world';
 
-file_get_contents("https://o8eqlgajbi-vpce-09bc0b188b0cf0095.execute-api.eu-west-2.amazonaws.com/sbx2");
+$gm_api = file_get_contents("https://o8eqlgajbi-vpce-09bc0b188b0cf0095.execute-api.eu-west-2.amazonaws.com/sbx2");
+echo $gm_api;
 ?>
