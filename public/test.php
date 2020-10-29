20e16f0a9e17e74d179dec5c2a55cee2559a7ee1
tommyb82 <thomasbunting@gmail.com>
2020-10-29 11:17:42 +0000

    Test agreements service connection

--- public/test.php
+++ public/test.php
@@ -9,7 +9,7 @@ if (!empty($_GET['debug'])) {
 var_dump($_GET);
 echo 'Hellor world';
 
-// Create a stream
+// Test conn to GM service
 $opts = array(
   'http'=>array(
     'method'=>"GET",
@@ -19,9 +19,20 @@ $opts = array(
 );
 
 $context = stream_context_create($opts);
-
-// Open the file using the HTTP headers set above
 $file = file_get_contents($_ENV["GUIDED_MATCH_SERVICE_ROOT_URL"]."/scale/guided-match-service/search-journeys/housing", false, $context);
 echo $file;
+
+// Test conn to agreements service
+$agr_opts = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Accept-language: en\r\n" .
+              "x-api-key: ".$_ENV["AGREEMENTS_SERVICE_API_KEY"]."\r\n"
+  )
+);
+
+$agr_context = stream_context_create($agr_opts);
+$agr_file = file_get_contents($_ENV["AGREEMENTS_SERVICE_ROOT_URL"]."/scale/agreements-service/agreements/", false, $agr_context);
+echo $agr_file;
 
 ?>
