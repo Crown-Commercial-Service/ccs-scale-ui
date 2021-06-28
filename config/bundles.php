395ef5aa8c4280d6b3a7e7c3e9a21b25c6a976a5
Bob <chee.ng@crowncommercial.gov.uk>
2021-06-28 15:47:56 +0100

    added rollbar config

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['local' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['local' => true],
+    Rollbar\Symfony\RollbarBundle\RollbarBundle::class => ['all' => true],
 ];
