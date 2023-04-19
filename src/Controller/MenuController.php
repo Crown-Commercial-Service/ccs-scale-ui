808d5dae091d39cc264cd83ef91f608fae2ed3ce
zubairali6 <zubair.ali@crowncommercial.gov.uk>
2023-04-19 12:54:15 +0100

    Update to Symfony 5 and fix errors

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use Psr\SimpleCache\CacheInterface;
-use Studio24\Frontend\Cms\Wordpress;
+use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Component\Cache\Psr16Cache;
+use Strata\Frontend\Cms\Wordpress;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -18,12 +19,13 @@ class MenuController extends AbstractController
      */
     protected $api;
 
-    public function __construct(CacheInterface $cache)
+    public function __construct(CacheItemPoolInterface $cache)
     {
         $this->api = new Wordpress(
             getenv('WEBCMS_ROOT_URL').'/wp-json/'
         );
-        $this->api->setCache($cache);
+        $psr16Cache = new Psr16Cache($cache);
+        $this->api->setCache($psr16Cache);
         $this->api->setCacheLifetime(9000);
     }
 
