0b5edd89855816c87abd0c4c985496370fa6effb
Chee-Ng <58430495+Chee-Ng@users.noreply.github.com>
2023-09-26 14:20:27 +0100

    ignore test folder and fix code climate

--- assets/scripts/dataLayer.js
+++ assets/scripts/dataLayer.js
@@ -1,7 +1,6 @@
 
 function pushDataLayer(interaction_type, link_text, interaction_detail, step){
 
-    console.log("dada");
     window.dataLayer.push({
         "event": 'gm_journey',
         "interaction_type":     interaction_type !== null ? interaction_type : null,
@@ -16,24 +15,10 @@ function questionType(type){
 
     switch(type){
         case 'list':     
-            for (var i = 0; i < document.getElementsByClassName('v-selector').length; i++) {
-                if (document.getElementsByClassName('v-selector')[i].checked){
-                    answer  = document.getElementsByClassName('v-selector')[i].labels[0].textContent;
-                    break;
-                }
-            }
+            answer = list();
             break;
         case 'multiSelect':
-                var checkboxes = document.getElementsByName('uuid[]');
-                var checkboxesChecked = [];
-    
-                for (var i=0; i<checkboxes.length; i++) {
-                    if (checkboxes[i].checked) {
-                        checkboxesChecked.push(checkboxes[i].labels[0].textContent);
-                    }
-                }
-    
-                answer = checkboxesChecked.join(' | ')
+            answer = multiSelect();
             break;
         case 'boolean':
             // we dont have any boolean quesiton on GM. They have all been archived.
@@ -43,7 +28,28 @@ function questionType(type){
     if(answer !== null){
         pushDataLayer('continue','continue',null,answer);
     }
+}
 
+function list(){
+    for (var i = 0; i < document.getElementsByClassName('v-selector').length; i++) {
+        if (document.getElementsByClassName('v-selector')[i].checked){
+            return document.getElementsByClassName('v-selector')[i].labels[0].textContent;
+        }
+    }
+    return null;
+}
+
+function multiSelect(){
+    var checkboxes = document.getElementsByName('uuid[]');
+    var checkboxesChecked = [];
+
+    for (var i=0; i<checkboxes.length; i++) {
+        if (checkboxes[i].checked) {
+            checkboxesChecked.push(checkboxes[i].labels[0].textContent);
+        }
+    }
+
+    return checkboxesChecked.join(' | ')
 }
 
 function printResultPage(){
